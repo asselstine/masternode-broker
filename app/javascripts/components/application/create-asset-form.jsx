9d5323ddac6fa0f45541329b8c5c8b448908debd
Brendan Asselstine <mail.asselstine@gmail.com>
2018-03-20 16:29:23 -0700

    Visual polish

--- app/javascripts/components/application/create-asset-form.jsx
+++ app/javascripts/components/application/create-asset-form.jsx
@@ -7,149 +7,153 @@ export default () => {
   return (
     <section className='section'>
       <div className='container is-fluid'>
-        <h1>Create Asset</h1>
-        <form className='columns'>
-          <div className='column is-four-fifths'>
+        <form className='columns is-centered'>
+          <div className='column is-two-thirds-desktop is-three-fifths-widescreen is-one-half-fullhd'>
 
-            <div className='field is-horizontal'>
-              <div className='field-label is-normal'>
-                <label className="label">ID</label>
-              </div>
-              <div className='field-body'>
-                <div className='field'>
-                  <p className='control is-expanded'>
-                    <input className='input' />
-                  </p>
+            <h1>Create Asset</h1>
+
+            <div className='box'>
+
+              <div className='field is-horizontal'>
+                <div className='field-label is-normal'>
+                  <label className="label">ID</label>
                 </div>
-              </div>
-            </div>
-
-            <div className='field is-horizontal'>
-              <div className='field-label is-normal'>
-                <label className="label">Type</label>
-              </div>
-              <div className='field-body'>
-                <div className='field has-addons'>
-                  <div className='control is-expanded'>
-                    <p className='select is-fullwidth'>
-                      <SelectAsset />
+                <div className='field-body'>
+                  <div className='field'>
+                    <p className='control is-expanded'>
+                      <input className='input' />
                     </p>
                   </div>
-                  <div className='control'>
-                    <label className='checkbox is-normal'>
-                      <input type='checkbox' /> Masternode
-                    </label>
+                </div>
+              </div>
+
+              <div className='field is-horizontal'>
+                <div className='field-label is-normal'>
+                  <label className="label">Type</label>
+                </div>
+                <div className='field-body'>
+                  <div className='field has-addons'>
+                    <div className='control is-expanded'>
+                      <p className='select is-fullwidth'>
+                        <SelectAsset />
+                      </p>
+                    </div>
+                    <div className='control'>
+                      <label className='checkbox is-normal'>
+                        <input type='checkbox' /> Masternode
+                      </label>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
 
-            <div className='field is-horizontal'>
-              <div className='field-label is-normal'>
-              </div>
-              <div className='field-body'>
-                <p className='help'>
-                  <small>Available: 474</small> <br />
-                  <small>Required: 1000</small>
-                </p>
-              </div>
-            </div>
-
-            <div className='field is-horizontal'>
-              <div className='field-label'>
-                <label className='label'>Buy</label>
-              </div>
-              <div className='field-body'>
-                <div className='field has-addons'>
-                  <div className='control is-expanded'>
-                    <input type='number' className='input' />
-                  </div>
-                  <p className='control'>
-                    <span className='button is-static'>
-                      Units
-                    </span>
+              <div className='field is-horizontal'>
+                <div className='field-label is-normal'>
+                </div>
+                <div className='field-body'>
+                  <p className='help'>
+                    <small>Available: 474</small> <br />
+                    <small>Required: 1000</small>
                   </p>
                 </div>
               </div>
-            </div>
 
-            <div className='field is-horizontal'>
-              <div className='field-label is-normal'>
-                <label className="label">Exchange</label>
-              </div>
-              <div className='field-body'>
-                <div className='field'>
-                  <p className='select is-fullwidth'>
-                    <select>
-                      <option>Automatic</option>
-                    </select>
-                  </p>
+              <div className='field is-horizontal'>
+                <div className='field-label'>
+                  <label className='label'>Buy</label>
                 </div>
-              </div>
-            </div>
-
-            <div className='field is-horizontal'>
-              <div className='field-label is-normal'>
-                <label className="label">Order</label>
-              </div>
-              <div className='field-body'>
-                <div className='field'>
-                  <p className='select is-fullwidth'>
-                    <select>
-                      <option>Alacundation</option>
-                    </select>
-                  </p>
-                </div>
-              </div>
-            </div>
-
-            <div className='field is-horizontal'>
-              <div className='field-label is-normal'>
-                <label className="label">Assignment</label>
-              </div>
-              <div className='field-body'>
-                <div className='field'>
-                  <p className='select is-fullwidth'>
-                    <select>
-                      <option>Unassigned</option>
-                      <option>4321 Company Inc.</option>
-                    </select>
-                  </p>
-                </div>
-              </div>
-            </div>
-
-            <div className='field is-horizontal'>
-              <div className='field-label is-normal'>
-                <label className="label">Mode</label>
-              </div>
-              <div className='field-body'>
-                <div className='field'>
-                  <p className='select is-fullwidth'>
-                    <select>
-                      <option>Accumulation</option>
-                      <option>Mode B</option>
-                      <option>Mode C</option>
-                    </select>
-                  </p>
-                </div>
-              </div>
-            </div>
-
-            <div className="field is-horizontal">
-              <div className="field-label">
-              </div>
-              <div className="field-body">
-                <div className="field">
-                  <div className="control">
-                    <Link to='/assets/confirm' className="button is-rust">
-                      Create Asset
-                    </Link>
+                <div className='field-body'>
+                  <div className='field has-addons'>
+                    <div className='control is-expanded'>
+                      <input type='number' className='input' />
+                    </div>
+                    <p className='control'>
+                      <span className='button is-static'>
+                        Units
+                      </span>
+                    </p>
                   </div>
                 </div>
               </div>
-            </div>
 
+              <div className='field is-horizontal'>
+                <div className='field-label is-normal'>
+                  <label className="label">Exchange</label>
+                </div>
+                <div className='field-body'>
+                  <div className='field'>
+                    <p className='select is-fullwidth'>
+                      <select>
+                        <option>Automatic</option>
+                      </select>
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className='field is-horizontal'>
+                <div className='field-label is-normal'>
+                  <label className="label">Order</label>
+                </div>
+                <div className='field-body'>
+                  <div className='field'>
+                    <p className='select is-fullwidth'>
+                      <select>
+                        <option>Alacundation</option>
+                      </select>
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className='field is-horizontal'>
+                <div className='field-label is-normal'>
+                  <label className="label">Assignment</label>
+                </div>
+                <div className='field-body'>
+                  <div className='field'>
+                    <p className='select is-fullwidth'>
+                      <select>
+                        <option>Unassigned</option>
+                        <option>4321 Company Inc.</option>
+                      </select>
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className='field is-horizontal'>
+                <div className='field-label is-normal'>
+                  <label className="label">Mode</label>
+                </div>
+                <div className='field-body'>
+                  <div className='field'>
+                    <p className='select is-fullwidth'>
+                      <select>
+                        <option>Accumulation</option>
+                        <option>Mode B</option>
+                        <option>Mode C</option>
+                      </select>
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="field is-horizontal">
+                <div className="field-label">
+                </div>
+                <div className="field-body">
+                  <div className="field">
+                    <div className="control">
+                      <Link to='/assets/confirm' className="button is-rust">
+                        Create Asset
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+            </div>
           </div>
         </form>
       </div>
--- app/javascripts/components/application/create-asset-success/index.jsx
+++ app/javascripts/components/application/create-asset-success/index.jsx
@@ -6,10 +6,14 @@ import style from './style'
 
 export default () => {
   return (
-    <section className='section create-asset-success'>
-      <div className='container is-fluid'>
-        <h1>Created Asset: NA123</h1>
-        <FontAwesome name='check-circle' className='create-asset-success__icon' />
+    <section className='section'>
+      <div className='container is-widescreen'>
+        <div className='columns is-centered'>
+          <div className='column is-two-thirds-desktop is-three-fifths-widescreen is-one-half-fullhd'>
+            <h1>Created Asset: NA123</h1>
+            <FontAwesome name='check-circle' className='create-asset-success__icon' />
+          </div>
+        </div>
       </div>
     </section>
   )
